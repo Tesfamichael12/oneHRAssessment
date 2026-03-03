1b290567bcf506df9a5bb5187a3542456a290d1d
Tesfamichael12 <tesfamichael132@gmail.com>
2026-03-03 10:07:18 +0300

    chore(init): bootstrap project configs and lint setup

---
+++ eslint.config.mjs
@@ -0,0 +1,32 @@
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+    ...compat.extends("next/core-web-vitals"),
+    {
+        rules: {
+            "no-console": ["error", { allow: ["warn", "error"] }],
+            "no-unused-vars": "off",
+            eqeqeq: ["error", "always"],
+            "max-lines": [
+                "warn",
+                {
+                    max: 500,
+                    skipBlankLines: true,
+                    skipComments: true,
+                },
+            ],
+            "no-duplicate-imports": "error",
+        },
+    },
+];
+
+export default eslintConfig;
